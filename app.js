a639e59055724d5937972840dcc4002b854cddc6
Akshay Kamble <kamblea635@gmail.com>
2021-12-09 20:14:30 +0530

    added minute functionality

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const secondHand = document.querySelector(".second-hand")
+const minHand = document.querySelector(".min-hand")
+const hourHand = document.querySelector(".hour-hand")
 
 function setDate(){
     const now = new Date()
     const seconds = now.getSeconds();
     const secondsDegree = ((seconds/60)*360)+90
     secondHand.style.transform = `rotate(${secondsDegree}deg)`
+
+    const mins = now.getMinutes()
+    const minsDegrees = ((mins/60)*360)+90;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`
     console.log(seconds)
+
 }
 
 setInterval(setDate,1000)
